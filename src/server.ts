e9f1e3c941696579a1b1390f93ae6af997131b5d
Ahshan-Haquc <ahshanulhaqucgub@gmail.com>
2026-05-09 11:12:49 +0600

    Added deployed frontend url origin

--- src/server.ts
+++ src/server.ts
@@ -26,6 +26,7 @@ app.use(
       "http://localhost:5174",
       "http://localhost:5175",
       "https://profilegen-cv-maker-frontend.vercel.app",
+      "https://profilegen-frontend.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
